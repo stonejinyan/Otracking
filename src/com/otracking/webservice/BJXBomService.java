0169dcc90eac8f602092e0eee897ae102e3fad08
stonejinyan <494902824@qq.com>
2020-11-11 15:03:30 +0800

    附件箱与SAP接口开发完成

--- src/com/otracking/webservice/BJXBomService.java
+++ src/com/otracking/webservice/BJXBomService.java
@@ -4,16 +4,16 @@ import com.otracking.dao.BJXBomDao;
 import com.otracking.tool.LogUtil;
 import com.otracking.webservice.bean.BJXBom;
 
-public class BjyBomService {
+public class BJXBomService {
 
 	BJXBomDao bjxBomDao = new BJXBomDao();
 
-	public BjyBomService() {
+	public BJXBomService() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public String importBjyBom(BJXBom bjxBom, String authentication) {
+	public String importBJXBom(BJXBom bjxBom, String authentication) {
 		LogUtil.RollingFile.info(authentication + "---BJXBOM:" + bjxBom);
 		return "success";
 	}
